eadeb025ad6e04f78e07cd97114a803b882dac47
KordyD <84672403+KordyD@users.noreply.github.com>
2024-01-30 20:00:46 +0300

    Update validation for access route

--- src/routes/access.routes.ts
+++ src/routes/access.routes.ts
@@ -10,11 +10,17 @@ const validator = [
     if (!value && !req.body.user_id) {
       throw new Error('Either group_id or user_id should not be empty');
     }
+    if (value && req.body.user_id) {
+      throw new Error('group_id and user_id should not be both provided');
+    }
     return true;
   }),
   body('user_id').custom((value, { req }) => {
     if (!value && !req.body.group_id) {
       throw new Error('Either group_id or user_id should not be empty');
+    }
+    if (value && req.body.group_id) {
+      throw new Error('group_id and user_id should not be both provided');
     }
     return true;
   }),
